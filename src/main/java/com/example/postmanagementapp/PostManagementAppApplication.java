fda4ab9418a8510f62f26fa8bedb31e3ad4e718f
Pedro <peuhenry2018@gmail.com>
2024-08-24 16:31:28 -0300

    Add DataBase MySQL

---
+++ src/main/java/com/example/postmanagementapp/PostManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.postmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postmanagementapp/PostManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
